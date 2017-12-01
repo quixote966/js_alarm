707fa56aad246543bd13dc81114cdd2280bd815f
willr <rhodesend@gmail.com>
2017-12-01 01:59:27 -0800

    Rebuilt app structure

--- js_alarm.js
+++ js_alarm.js
@@ -1,68 +1,112 @@
 /**
  * @author Will Rhodes
  */
-var alarms = new Array();
 
-function heartbeat() {
-	// Send a new message every second, containing a new Date Object for comparison
-	console.log('lubdub');
-	var beat = new Date();
-	setTimeout (function () {
-		updateClockView(date);
-		checkAlarms(date);
-		heartbeat();
-	}, 1000);
-}
 
-function Alarm(hr,min) {
-	this.hour = hr;
-	this.minute = min;
+function Alarm(hr,min,alarmsDiv) {
+//	this.hour = hr;
+//	this.minute = min;
+	this.time = new Date(0);
+	this.time.setHours(hr, min);
 	this.fired = false;
-	t = this;
+	this.alarmView=new AlarmView(this.time,alarmsDiv);
 	// TODO should handle AM/PM and locale
+	
+	this.equal = function(date) {
+		return date.getHours() === this.time.getHours() && date.getMinutes() === this.time.getMinutes();
+	};
+	
+	this.check = function(date) {
+		if (this.equal(date) && !this.fired) {
+			console.log("the alarm has happened");
+			this.fired = true;
+			this.alarmView.fire();
+		}
+	};
+	
 }
 
-Alarm.prototype.equal = function(date) {
-	return date.getHours() === this.hour && date.getMinutes() === this.minute;
-};
+function ClockView() {
+	this.clockDiv = document.getElementById("clock");
+	
+	this.update = function(date) {
+		this.clockDiv.textContent = date.toLocaleTimeString("en-US");
+	};
+}
 
-Alarm.prototype.check = function(date) {
-	if (t.equal(date) && !this.fired) {
-		console.log("the alarm has happened");
-		window.alert("the alarm has happened!");
-		this.fired = true;
+function AlarmView(time,alarmsDiv) {
+	this.time = time;
+	this.elem = document.createElement('div');
+	this.elem.classList.add('alarm_view');
+	this.elem.textContent = time.toLocaleTimeString("en-US");
+	alarmsDiv.appendChild(this.elem);
+	
+	this.fire = function() {
+		// TODO Make this do more. Also, prep the CSS
+		this.elem.classList.add('alarm_firing');
+	};
+	
+	this.cancel = function () {
+		this.elem.classList.remove('alarm_firing');
+	};
+
+}
+
+
+
+function AlarmClock() {
+	this.alarms = new Array();
+	this.clockView = new ClockView();
+	console.log(this.clockView);
+	this.alarmsDiv = document.getElementById("alarmsList");
+	var parent = this;
+	this.heartbeat = function() {
+		console.log('lubdub');
+		var beat = new Date();
+		setTimeout (function () {
+			parent.clockView.update(beat);
+			parent.checkAlarms(beat);
+			parent.heartbeat();
+		}, 1000);
+	};
+
+	this.checkAlarms = function(date) {
+		this.alarms.forEach(function(alarm) {
+			console.log(date);
+			alarm.check(date);
+		});
+	};
+
+	this.setAlarm = function (hr,min) {
+		console.log("setting alarm "+hr+":"+min);
+		if (hr >= 0 && 
+			hr < 24 &&
+			min >= 0 &&
+			min < 60) {
+			// create an alarm object.
+			alarm = new Alarm(hr,min,this.alarmsDiv);
+			this.alarms.push(alarm);
+			
+		} else {
+			// TODO display an error.
+		}
 	}
-};
 
-
-function setAlarm(hr,min) {
-	console.log("setting alarm "+hr+":"+min);
-	if (hr >= 0 && 
-		hr < 24 &&
-		min >= 0 &&
-		min < 60) {
-		// create an alarm object.
-		alarm = new Alarm(hr,min);
-		alarms.push(alarm);
-		// TODO update view
-		
-	} else {
-		// TODO display an error.
-	}
 }
 
-function alarmView()
-
-var checkAlarms = function(date) {
-	alarms.forEach(function(alarm) {
-		console.log(date);
-		alarm.check(date);
-	});
-}
 
 document.onreadystatechange = function() {
 	  if(document.readyState === "interactive") {
-		  heartbeat();
+		  var app=new AlarmClock();
+		  console.log("app?");
+		  var form = document.getElementById("alarmForm");
+		  form.addEventListener("submit",function(event) {
+			  event.preventDefault();
+			  var hour = parseInt(form.hour.value);
+			  var min = parseInt(form.minute.value);
+			  app.setAlarm(hour, min);
+		  });
+		  app.heartbeat();
 	  }
 };
 
